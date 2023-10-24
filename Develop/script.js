80ec22a48b9f5eab4f5d35b3055e210d2102bd5a
Jorge Duarte <sadujorge@gmail.com>
2023-10-23 23:03:25 -0700

    Switched radio and checkbox inputs to select inputs. Can now retrieve values from inputs.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,26 @@
 "use strict";
+// Global Variables
+let charLength;
+let specialChars;
+let uppercase;
+let lowercase;
+let numeric;
 // Selected Elements
 const pwForm = document.querySelector('#passwordGeneratorForm');
 const pwOutput = document.querySelector('#generatedPassword');
+const charLengthSelect = document.querySelector('#passwordLength');
+const lowercaseSelect = document.querySelector('#lowercase');
+const uppercaseSelect = document.querySelector('#uppercase');
+const specialSelect = document.querySelector('#specialCharacters');
+const numberSelect = document.querySelector('#numbers');
 // Event Listeners
 pwForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const generatedPassword = "Works!";
+    charLength = charLengthSelect.value;
+    lowercase = lowercaseSelect.value === 'true';
+    uppercase = uppercaseSelect.value === 'true';
+    specialChars = specialSelect.value === 'true';
+    numeric = numberSelect.value === 'true';
+    const generatedPassword = `${charLength},${lowercase},${uppercase},${specialChars},${numeric}, `;
     pwOutput.value = generatedPassword;
 });
--- script.ts
+++ script.ts
@@ -1,9 +1,27 @@
+// Global Variables
+let charLength: string;
+let specialChars: boolean;
+let uppercase: boolean;
+let lowercase: boolean;
+let numeric: boolean;
 // Selected Elements
 const pwForm = document.querySelector('#passwordGeneratorForm') as HTMLButtonElement;
 const pwOutput = document.querySelector('#generatedPassword') as HTMLTextAreaElement;
+const charLengthSelect = document.querySelector('#passwordLength') as HTMLSelectElement;
+const lowercaseSelect = document.querySelector('#lowercase') as HTMLSelectElement;
+const uppercaseSelect = document.querySelector('#uppercase') as HTMLSelectElement;
+const specialSelect = document.querySelector('#specialCharacters') as HTMLSelectElement;
+const numberSelect = document.querySelector('#numbers') as HTMLSelectElement;
 // Event Listeners
 pwForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const generatedPassword: string = "Works!";
+
+    charLength = charLengthSelect.value;
+    lowercase = lowercaseSelect.value === 'true';
+    uppercase = uppercaseSelect.value === 'true';
+    specialChars = specialSelect.value === 'true';
+    numeric = numberSelect.value === 'true';
+
+    const generatedPassword: string = `${charLength},${lowercase},${uppercase},${specialChars},${numeric}, `;
     pwOutput.value = generatedPassword;
 })
