9af0509ddbffa8db1a9bdcd88c08f0ce09bf5c5b
Jorge Duarte <sadujorge@gmail.com>
2023-10-23 22:19:05 -0700

    Added event listener to form

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Hello");
+// Selected Elements
+const pwForm = document.querySelector('#passwordGeneratorForm');
+const pwOutput = document.querySelector('#generatedPassword');
+// Event Listeners
+pwForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const generatedPassword = "Works!";
+    pwOutput.value = generatedPassword;
+});
--- script.ts
+++ script.ts
@@ -1,1 +1,9 @@
-console.log("Hello")
+// Selected Elements
+const pwForm = document.querySelector('#passwordGeneratorForm') as HTMLButtonElement;
+const pwOutput = document.querySelector('#generatedPassword') as HTMLTextAreaElement;
+// Event Listeners
+pwForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const generatedPassword: string = "Works!";
+    pwOutput.value = generatedPassword;
+})
